fa6c36b67894b6e56439e5894f5c6b6175cda04a
eekrwl <wani9909@naver.com>
2024-08-02 15:07:52 +0900

    KKUMI-61 [FEAT] #61 포스트 등록 기능

--- src/main/java/com/swmarastro/mykkumiserver/MykkumiServerApplication.java
+++ src/main/java/com/swmarastro/mykkumiserver/MykkumiServerApplication.java
@@ -2,7 +2,9 @@ package com.swmarastro.mykkumiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MykkumiServerApplication {
     public static void main(String[] args) {
--- src/main/java/com/swmarastro/mykkumiserver/post/service/PostService.java
+++ src/main/java/com/swmarastro/mykkumiserver/post/service/PostService.java
@@ -1,25 +1,36 @@
 package com.swmarastro.mykkumiserver.post.service;
 
+import com.swmarastro.mykkumiserver.category.CategoryService;
+import com.swmarastro.mykkumiserver.category.domain.SubCategory;
 import com.swmarastro.mykkumiserver.global.exception.CommonException;
 import com.swmarastro.mykkumiserver.global.exception.ErrorCode;
 import com.swmarastro.mykkumiserver.global.util.Base64Utils;
 import com.swmarastro.mykkumiserver.post.PostLatestCursor;
 import com.swmarastro.mykkumiserver.post.PostRepository;
+import com.swmarastro.mykkumiserver.post.domain.Pin;
 import com.swmarastro.mykkumiserver.post.domain.Post;
-import com.swmarastro.mykkumiserver.post.dto.PostDto;
-import com.swmarastro.mykkumiserver.post.dto.PostListResponse;
+import com.swmarastro.mykkumiserver.post.domain.PostImage;
+import com.swmarastro.mykkumiserver.post.dto.*;
+import com.swmarastro.mykkumiserver.post.richtext.PostContentObject;
+import com.swmarastro.mykkumiserver.post.richtext.RichTextUtils;
+import com.swmarastro.mykkumiserver.product.Product;
+import com.swmarastro.mykkumiserver.user.User;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PostService {
 
     private final PostRepository postRepository;
+    private final CategoryService categoryService;
 
     //TODO 이름에 최신순이라고 알려줘야할것 같다. 고민해보고 이름 고치기
     public PostListResponse getInfiniteScrollPosts(String encodedCursor, Integer limit) {
@@ -30,7 +41,6 @@ public class PostService {
         //TODO Cursor 인터페이스 하나 놓고 상속해서 커스텀하여 사용할 수도 있을 것 같다. 추후 구조 수정
         PostLatestCursor cursor = getCursorFromBase64String(encodedCursor);
         List<PostDto> posts = getPostsByCursorAndLimit(cursor, limit + 1);
-
         if (posts.size() < limit + 1) { //다음에 조회할 내용 없음
             return PostListResponse.end(posts);
         }
@@ -38,6 +48,32 @@ public class PostService {
         Long lastId = getLastIdFromPostList(posts);
         PostLatestCursor nextCursor = PostLatestCursor.of(cursor.getStartedAt(), lastId);
         return PostListResponse.of(posts, Base64Utils.encode(nextCursor));
+    }
+
+    public Long registerPost(User user, Long subCategoryId, String content, List<PostImageDto> images) {
+        //content -> rich text로 변환
+        List<String> contentTexts = RichTextUtils.extractPlainTextAndHashtags(content);
+        List<PostContentObject> postContentObjects = RichTextUtils.makePostContentStringToRichText(contentTexts);
+        SubCategory subCategory = categoryService.getSubCategoryById(subCategoryId);
+
+        List<PostImage> postImages = new ArrayList<>();
+        Long order=1L;
+        for (PostImageDto postImageDto : images) {
+            PostImage postImage = PostImage.of(postImageDto.getUrl(), order++);
+            List<Pin> pins = postImageDto.getPins().stream()
+                    .map(pinDto -> {
+                        Product product = Product.of(pinDto.getProductInfo().getName(), pinDto.getProductInfo().getUrl());
+                        return Pin.of(postImage, product, pinDto.getPositionX(), pinDto.getPositionY());
+                    })
+                    .collect(Collectors.toList());
+            postImage.addPins(pins);
+            postImages.add(postImage);
+        }
+
+        Post post = Post.of(postContentObjects, user, subCategory, postImages);
+        Post savedPost = postRepository.save(post);
+
+        return savedPost.getId();
     }
 
     private PostLatestCursor getCursorFromBase64String(String encodedCursor) {
